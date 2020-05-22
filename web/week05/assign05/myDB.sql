eaa604ef7a5b2279f8298d5f1c3ef290b3545ee3
gmoffett96 <51720777+gmoffett96@users.noreply.github.com>
2020-05-21 18:32:58 -0600

    Add files via upload

---
+++ web/week05/assign05/myDB.sql
@@ -0,0 +1,14 @@
+Insert Into users (user_id, firstName, lastName, age)
+VALUES (1, 'John', 'Smith', 30),
+    (2, 'William', 'Limber', 45),
+    (3, 'Seth', 'Ander', 22),
+    (4, 'George', 'Smith', 35),
+    (5, 'Lucy', 'Denis', 37);
+
+Insert Into finances (user_id, currentAmount, currentObligations, monthlyIncome, tithing, 
+entertainment, bills, other)
+Values (1, 100000, -3000, 5000, 500, 400, 1000, 0),
+    (2, 0, 0, 0, 0, 0, 0, 0),
+    (3, 50000, -2000, 4000, 400, 300, 2000, 700),
+    (4, 2000000000, -5000000, 10000, 1000, 8000, 0, 5000),
+    (5, 300, 150, 500, 50, 30, 250, 250);
